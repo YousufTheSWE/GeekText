93999a536d2c2acd28c8baed6a5f29585e1e4097
Jason <jasongms2017@gmail.com>
2024-05-30 15:16:31 -0400

    First commit

---
+++ src/main/java/com/example/geekText/library/BookController.java
@@ -0,0 +1,24 @@
+package com.example.geekText.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return bookService.getBooks();
+    }
+}
