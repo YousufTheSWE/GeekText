697fd035e4a616490e53610a3257be4462147f45
YousufTheSWE <yousufh2006@gmail.com>
2024-06-22 17:24:28 -0400

    Added proper errors and app.prop

--- src/main/java/com/example/geekText/CommentLibrary/CommentService.java
+++ src/main/java/com/example/geekText/CommentLibrary/CommentService.java
@@ -1,6 +1,8 @@
 package com.example.geekText.CommentLibrary;
 
+import com.example.geekText.BookLibrary.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
@@ -9,6 +11,11 @@ import java.util.List;
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
+    @Autowired
+    private ApplicationContext appContext;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @Autowired
     public CommentService(CommentRepository commentRepository) {
@@ -23,6 +30,8 @@ public class CommentService {
         comment.setDatestamp(ZonedDateTime.now());
         if (comment.getBookId() == null || comment.getUserId() == null || comment.getComment() == null)
             throw new IllegalArgumentException("Needs a bookId and a userId");
+        if (!bookRepository.findById(comment.getBookId()).isPresent())
+            throw new IllegalStateException("book with id " + comment.getBookId() + " does not exist");
         commentRepository.save(comment);
     }
 }
