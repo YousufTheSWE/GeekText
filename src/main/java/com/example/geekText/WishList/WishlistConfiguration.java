67545c2ee917e5cb8b868bae79ef409f280441c3
williamguerrero <wizwang720@gmail.com>
2024-07-20 10:06:26 -0400

    Wishlist Management package fixed

--- src/main/java/com/example/geekText/WishList/WishlistConfiguration.java
+++ src/main/java/com/example/geekText/WishList/WishlistConfiguration.java
@@ -1,34 +1,51 @@
 package com.example.geekText.WishList;
 
-import com.example.geekText.BookLibrary.Book;
+import com.example.geekText.Profile.ProfileRepository;
+import com.example.geekText.Profile.Profiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@Order(3)
 public class WishlistConfiguration {
 
     @Bean
-    CommandLineRunner commandLineRunner2(
-            WishlistRepository repository){
+    CommandLineRunner WishlistCommandLineRunner(
+            ProfileRepository profileRepository, WishlistRepository repository){
         return args -> {
-            String[] myArray = Arrays.asList("the").toArray(new String[0]);
-            Wishlist horror;
-            horror = new Wishlist(
-                    "Horror",
-                    1L,
-                    null);
-            Wishlist stealth = new Wishlist(
-                    "Slice of Life",
-                    2L,
-                    null);
+
+
+
+
+            Profiles jimmy = profileRepository.findById(1L)
+                    .orElseThrow(() -> new RuntimeException("Profile not found"));
+
+            Profiles michael = profileRepository.findById(2L)
+                    .orElseThrow(() -> new RuntimeException("Profile not found"));
+
+
+
+
+
+            Wishlist wishlist1 = new Wishlist(
+                    "Christmas Wishlist",
+                    jimmy
+                    );
+            Wishlist wishlist2 = new Wishlist(
+                    "Birthday Wishlist",
+                    michael
+                    );
 
             repository.saveAll(
-                    List.of(horror, stealth)
+                    List.of(wishlist1, wishlist2)
             );
+
 
 
         };
